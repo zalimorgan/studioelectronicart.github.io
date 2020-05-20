b4160e01e957957b6c2c4ce3a3c7b9fb5e3f1776
Braden Hinkle <61490647+beeeffjee@users.noreply.github.com>
2020-05-20 16:25:58 +0800

    Add files via upload

---
+++ students/bradenh/RandomTest/RandomTest.js
@@ -0,0 +1,24 @@
+
+
+
+function makeLink() {
+	var result = '';
+	
+	var characters='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-';
+    
+	var charactersLength = characters.length;
+	
+	for ( var i = 0; i < 11; i++)
+	{result += characters.charAt(Math.floor(Math.random() * charactersLength));}
+	
+	var myUrl = "https://www.youtube.com/embed/"; 
+		myUrl += result;
+		myUrl += "?autoplay=1"
+		return myUrl;
+}
+// += "?autoplay=1"
+
+document.getElementById("specialDiv1").innerText = ();
+
+document.getElementById("specialDiv2").innerText = (makeLink());
+	  
