0c1660258fc465be50e6636cd33351659ad88704
Studio Electronic Art <61098327+studioelectronicart@users.noreply.github.com>
2020-05-20 16:33:02 +0800

    updates to randomtest js

--- students/bradenh/RandomTest/RandomTest.js
+++ students/bradenh/RandomTest/RandomTest.js
@@ -1,24 +1,27 @@
+$(document).ready(function(){
+	makeLink();
+	$('iframe').attr('src', myUrl);
 
+});
 
 
 function makeLink() {
 	var result = '';
-	
+
 	var characters='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-';
-    
+
 	var charactersLength = characters.length;
-	
+
 	for ( var i = 0; i < 11; i++)
 	{result += characters.charAt(Math.floor(Math.random() * charactersLength));}
-	
-	var myUrl = "https://www.youtube.com/embed/"; 
+
+	var myUrl = "https://www.youtube.com/embed/";
 		myUrl += result;
-		myUrl += "?autoplay=1"
+		myUrl += "?autoplay=1";
 		return myUrl;
 }
 // += "?autoplay=1"
 
-document.getElementById("specialDiv1").innerText = ();
+//document.getElementById("specialDiv1").innerText = ();
 
-document.getElementById("specialDiv2").innerText = (makeLink());
-	  
+//document.getElementById("specialDiv2").innerText = (makeLink());
