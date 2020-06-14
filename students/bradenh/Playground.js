67317251fc6b3d7a307fa4527b96bd3e5a55e23f
Braden Hinkle <61490647+beeeffjee@users.noreply.github.com>
2020-06-15 03:04:22 +0800

    Add files via upload

--- students/bradenh/Playground.js
+++ students/bradenh/Playground.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
-    animateDiv();
-    
+    animateDiv('.a');
+    animateDiv('.b');
+    animateDiv('.c');
+    animateDiv('.d');
 });
 
 function makeNewPosition(){
@@ -16,28 +18,10 @@ function makeNewPosition(){
     
 }
 
-function animateDiv(){
+function animateDiv(myclass){
     var newq = makeNewPosition();
-    var oldq = $('.a').offset();
-    var speed = calcSpeed([oldq.top, oldq.left], newq);
-    
-    $('.a').animate({ top: newq[0], left: newq[1] }, speed, function(){
-      animateDiv();        
+    $(myclass).animate({ top: newq[0], left: newq[1] }, 1000,   function(){
+      animateDiv(myclass);        
     });
     
 };
-
-function calcSpeed(prev, next) {
-    
-    var x = Math.abs(prev[1] - next[1]);
-    var y = Math.abs(prev[0] - next[0]);
-    
-    var greatest = x > y ? x : y;
-    
-    var speedModifier = 0.1;
-
-    var speed = Math.ceil(greatest/speedModifier);
-
-    return speed;
-
-}// JavaScript Document
